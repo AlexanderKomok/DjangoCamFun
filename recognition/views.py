1529719db762ea99a2e1801fb4fb800ec73ecb07
Alexander Komok <alexanderkomok1999@gmail.com>
2024-10-27 23:24:14 +0200

    partyally switched from django orm to sql

--- recognition/views.py
+++ recognition/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from recognition.models import PlateEvent, Camera
+from django.db import connection
 from django.utils.dateparse import parse_datetime
 
 def search(request):
@@ -8,23 +8,76 @@ def search(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
     results = []
+    cameras = []
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, name FROM recognition_camera")
+        cameras = cursor.fetchall()
 
     if start_date and end_date:
         start_datetime = parse_datetime(f'{start_date} 00:00:00')
         end_datetime = parse_datetime(f'{end_date} 23:59:59')
+    
+    #только номер
+    if query and not camera_id and not (start_date and end_date):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pe.plate_number, pe.brand, pe.color, pe.recognition_time,
+                       c.name, c.ip_address, c.latitude, c.longitude
+                FROM recognition_plateevent pe
+                JOIN recognition_camera c ON pe.camera_id = c.id
+                WHERE pe.plate_number ILIKE %s
+            """, [f'%{query}%'])
+            results = cursor.fetchall()
 
-        if query:
-            results = PlateEvent.objects.filter(
-                plate_number__icontains=query,
-                recognition_time__range=[start_datetime, end_datetime]
-            )
+    #только камера
+    elif camera_id and not query and not (start_date and end_date):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pe.plate_number, pe.brand, pe.color, pe.recognition_time,
+                       c.name, c.ip_address, c.latitude, c.longitude
+                FROM recognition_plateevent pe
+                JOIN recognition_camera c ON pe.camera_id = c.id
+                WHERE pe.camera_id = %s
+            """, [camera_id])
+            results = cursor.fetchall()
 
-        elif camera_id:
-            results = PlateEvent.objects.filter(
-                camera_id=camera_id,
-                recognition_time__range=[start_datetime, end_datetime]
-            )
+    #номер и диапазон дат
+    elif query and start_date and end_date:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pe.plate_number, pe.brand, pe.color, pe.recognition_time,
+                       c.name, c.ip_address, c.latitude, c.longitude
+                FROM recognition_plateevent pe
+                JOIN recognition_camera c ON pe.camera_id = c.id
+                WHERE pe.plate_number ILIKE %s
+                AND pe.recognition_time BETWEEN %s AND %s
+            """, [f'%{query}%', start_datetime, end_datetime])
+            results = cursor.fetchall()
 
-    cameras = Camera.objects.all()
-    #print(cameras)
+    #камера и диапазон дат
+    elif camera_id and start_date and end_date:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pe.plate_number, pe.brand, pe.color, pe.recognition_time,
+                       c.name, c.ip_address, c.latitude, c.longitude
+                FROM recognition_plateevent pe
+                JOIN recognition_camera c ON pe.camera_id = c.id
+                WHERE pe.camera_id = %s
+                AND pe.recognition_time BETWEEN %s AND %s
+            """, [camera_id, start_datetime, end_datetime])
+            results = cursor.fetchall()
+
+    #диапазон дат
+    elif start_date and end_date and not query and not camera_id:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pe.plate_number, pe.brand, pe.color, pe.recognition_time,
+                       c.name, c.ip_address, c.latitude, c.longitude
+                FROM recognition_plateevent pe
+                JOIN recognition_camera c ON pe.camera_id = c.id
+                WHERE pe.recognition_time BETWEEN %s AND %s
+            """, [start_datetime, end_datetime])
+            results = cursor.fetchall()
+
     return render(request, 'search.html', {'results': results, 'cameras': cameras})
